065a9630d0529e028bb7c247385031b76b38a29d
qymiy <2718106017@qq.com>
2023-05-16 20:00:06 +0800

    接口文档完成

--- ScholarSphere/article/views.py
+++ ScholarSphere/article/views.py
@@ -2,7 +2,7 @@ from django.shortcuts import render
 from article.models import Work
 # Create your views here.
 def article_get_by_name(article_name):
-    return Work.objects.filter(work_name=article_name)
+    return Work.objects.filter(work_name__icontains=article_name)
 
 def article_get_by_id(article_id):
     return Work.objects.filter(id=article_id)
